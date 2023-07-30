081caeaf0fc570d0e5a1076e06a93e96bcb6e84d
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-07-30 18:10:13 +0900

    Style : KeyBoard 컴포넌트 작성

--- src/components/KeyBoard.tsx
+++ src/components/KeyBoard.tsx
@@ -1,5 +1,71 @@
+import { styled } from "styled-components";
+
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
 const KeyBoard = () => {
-  return <div>KeyBoard</div>;
+  return (
+    <div style={{ alignSelf: "stretch" }}>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit,minmax(75px,1fr))",
+          gap: "0.5rem",
+        }}>
+        {KEYS.map((key) => {
+          return <Button key={key}>{key}</Button>;
+        })}
+      </div>
+    </div>
+  );
 };
+
+const Button = styled.button`
+  width: 100%;
+  border: 3px solid black;
+  background: none;
+  aspect-ratio: 1 / 1;
+  font-size: 2.5rem;
+  text-transform: uppercase;
+  padding: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  color: black;
+  &:active {
+    background-color: hsl(200, 100%, 50%);
+    color: white;
+  }
+
+  &:hover:not(:disabled),
+  &:focus:not(:disabled) {
+    background-color: hsl(200, 100%, 75%);
+  }
+`;
 
 export default KeyBoard;
