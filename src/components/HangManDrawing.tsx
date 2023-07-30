04893e05afae7fc493a5ab4cc0f81709f2f48fe1
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-07-30 21:52:45 +0900

    Fix : 컴포넌트 수정

--- src/components/HangManDrawing.tsx
+++ src/components/HangManDrawing.tsx
@@ -1,14 +1,104 @@
 import { styled } from "styled-components";
 
-const HangManDrawing = () => {
+const HangManDrawing = ({ numberOfGuess }: { numberOfGuess: number }) => {
+  const HEAD = (
+    <div
+      key="head"
+      style={{
+        width: "50px",
+        height: "50px",
+        borderRadius: "100%",
+        border: "10px solid black",
+        position: "absolute",
+        top: "50px",
+        right: "-30px",
+      }}
+    />
+  );
+
+  const BODY = (
+    <div
+      key="body"
+      style={{
+        width: "10px",
+        height: "100px",
+        background: "black",
+        position: "absolute",
+        top: "120px",
+        right: 0,
+      }}
+    />
+  );
+
+  const RIGHT_ARM = (
+    <div
+      key="rightArm"
+      style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        top: "150px",
+        right: "-100px",
+        rotate: "-30deg",
+        transformOrigin: "left bottom",
+      }}
+    />
+  );
+
+  const LEFT_ARM = (
+    <div
+      key="leftArm"
+      style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        top: "150px",
+        right: "10px",
+        rotate: "30deg",
+        transformOrigin: "right bottom",
+      }}
+    />
+  );
+
+  const RIGHT_LEG = (
+    <div
+      key="rightLeg"
+      style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        top: "210px",
+        right: "-90px",
+        rotate: "60deg",
+        transformOrigin: "left bottom",
+      }}
+    />
+  );
+
+  const LEFT_LEG = (
+    <div
+      key="leftLeg"
+      style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        top: "210px",
+        right: 0,
+        rotate: "-60deg",
+        transformOrigin: "right bottom",
+      }}
+    />
+  );
+
+  const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]; //   const hangMan = [
+
   return (
     <HangManDrawingContainer>
-      <Head />
-      <Body />
-      <RightLeg />
-      <LeftLeg />
-      <RightArm />
-      <LeftArm />
+      {BODY_PARTS.slice(0, numberOfGuess)}
       <RightTopBar />
       <TopBar />
       <InnerTopDrawer></InnerTopDrawer>
