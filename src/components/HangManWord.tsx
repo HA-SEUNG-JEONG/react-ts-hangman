ed95702abe26d8acf2aa16040fc015f915c81bcb
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-07-30 18:09:41 +0900

    Style : HangManWord 컴포넌트 작성

--- src/components/HangManWord.tsx
+++ src/components/HangManWord.tsx
@@ -1,5 +1,33 @@
+import { styled } from "styled-components";
+
 const HangManWord = () => {
-  return <div>HangManWord</div>;
+  const word = "tee";
+  const guessedLetters = ["t", "e", "g"];
+  return (
+    <HangManWordContainer>
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: "0.1rem solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}>
+            {letter}
+          </span>
+        </span>
+      ))}
+    </HangManWordContainer>
+  );
 };
+
+const HangManWordContainer = styled.div`
+  display: flex;
+  gap: 0.25rem;
+  font-size: 6rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-family: monospace;
+`;
 
 export default HangManWord;
