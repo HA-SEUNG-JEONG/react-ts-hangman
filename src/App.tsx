8dba2173e1f1e59eb2dc2ff21edd7437991dc804
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-07-30 22:02:27 +0900

    Feat : 기능 추가 - 키보드 감지 처리 - 키보드 입력시 단어 추가가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import words from "./wordList.json";
 import styled from "styled-components";
@@ -7,16 +7,79 @@ import HangManWord from "./components/HangManWord";
 import KeyBoard from "./components/KeyBoard";
 
 function App() {
-  const [wordGuess, setWordGuess] = useState(
-    () => words[Math.floor(Math.random() * words.length)]
-  );
+  const getWord = () => words[Math.floor(Math.random() * words.length)];
+
+  const [wordGuess, setWordGuess] = useState(getWord);
+  console.log(wordGuess);
   const [guessedLetter, setGuessLetter] = useState<string[]>([]);
+
+  const inCorrectLetters = guessedLetter.filter(
+    (letter) => !wordGuess.includes(letter)
+  );
+
+  const isLoser = inCorrectLetters.length >= 6;
+  const isWinner = wordGuess
+    .split("")
+    .every((letter) => guessedLetter.includes(letter));
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetter.includes(letter) || isWinner || isLoser) return;
+      setGuessLetter((prev) => [...prev, letter]);
+    },
+    [guessedLetter, isWinner, isLoser]
+  );
+
+  useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      const key = event.key;
+      if (!key.match(/^[a-z]$/)) return;
+      event.preventDefault();
+      addGuessedLetter(key);
+    };
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetter, addGuessedLetter]);
+
+  useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      const key = event.key;
+
+      if (key !== "Enter") return;
+      event.preventDefault();
+
+      setWordGuess(getWord());
+    };
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, []);
+
   return (
     <Container>
-      <Status>Lose Win</Status>
-      <HangManDrawing />
-      <HangManWord />
-      <KeyBoard />
+      <Status>{isWinner && "Winner"}</Status>
+      <Status>{isLoser && "Losers"}</Status>
+      <HangManDrawing numberOfGuess={inCorrectLetters.length} />
+      <HangManWord
+        reveal={isLoser}
+        guessedLetter={guessedLetter}
+        words={wordGuess}
+      />
+      <KeyBoard
+        disabled={isWinner || isLoser}
+        activeLetter={guessedLetter.filter((letter) =>
+          wordGuess.includes(letter)
+        )}
+        inactiveLetter={inCorrectLetters}
+        addGuessedLetter={addGuessedLetter}
+      />
     </Container>
   );
 }
